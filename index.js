e278a64d1a57f88bd2df0c98a8416dc323cffa4f
Alexandre Roba <alexandre.roba@arobanet.be>
2014-02-03 23:00:45 +0100

    Added the structure for the repository

--- index.js
+++ index.js
@@ -2,6 +2,6 @@ module.exports = {
     VERSION: require('./package.json').version,
     uuid: require('./lib/utils').uuid,
 //    domain: require('./lib/domain'),
-    AggregateRoot: require('./lib/base/aggregateRoot')
-
+    AggregateRoot: require('./lib/base/aggregateRoot'),
+    Repository: require('./lib/base/repository')
 };
